0292a8c23f5094b59689b540054303748462da20
Marcílio Sousa <marcilio.ec.ufc@gmail.com>
2019-02-14 23:39:58 -0300

    Adicionar arquivos de projeto.

---
+++ PDFReader/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PDFReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(775, 296);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(689, 315);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(99, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Choose File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ PDFReader/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDFReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF Files|*.pdf", ValidateNames = true, Multiselect = false })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        PdfReader reader = new PdfReader(ofd.FileName);
+                        List<string> questoes = new List<string>();
+                        int numQuestoes = 0;
+                        //StringBuilder sb = new StringBuilder();
+
+                        for (int i=1; i<=reader.NumberOfPages; i++)
+                        {
+                            //string pattern = "(\n\\d{2})(.+\n+)+?(\\(E\\)).+";
+
+                            StringBuilder sb = new StringBuilder();
+
+                            sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+
+                            string pageText = sb.ToString();
+                            richTextBox1.Text += pageText;
+
+                            /*while (Regex.IsMatch(pageText, pattern))
+                            {
+                                foreach(Match match in Regex.Matches(pageText, pattern))
+                                {
+                                    pageText = Regex.Replace(pageText, pattern, string.Empty);
+                                    numQuestoes++;
+                                    richTextBox1.Text += match.Value + '\n';
+                                    sb.Replace(match.Value, string.Empty);
+                                }
+                            }*/
+                        }
+
+                        //MessageBox.Show("Numero de questoes - " + numQuestoes.ToString());
+
+                        reader.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
